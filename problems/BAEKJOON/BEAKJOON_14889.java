4876e93b25f3ec21c000f181671e3826bff3024a
commitkim <65940401+commitkim@users.noreply.github.com>
2020-09-02 03:18:00 +0900

    백준 14889. 스타트와 링크 / 프로그래머스 2019 블라인드 후보키
    백준 14889. 스타트와 링크
    -조합을 사용한 브루트포스
    프로그래머스 2019 블라인드 후보키
    -조합과 Set을 사용

--- problems/BAEKJOON/BEAKJOON_14889.java
+++ problems/BAEKJOON/BEAKJOON_14889.java
@@ -1,5 +1,94 @@
 package problems.BAEKJOON;
 
-public class BEAKJOON_14889 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
+public class BEAKJOON_14889 {
+	private static int N, answer;
+	private static int[] selected;
+	private static int[][] S;
+
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	private static boolean nextPermutation(int[] selected) {
+		int i = N - 1;
+		while (i > 0 && selected[i - 1] >= selected[i])
+			--i;
+
+		if (i == 0)
+			return false;
+
+		int j = N - 1;
+		while (selected[i - 1] >= selected[j])
+			--j;
+
+		swap(selected, i - 1, j);
+
+		int k = N - 1;
+		while (k > i) {
+			swap(selected, k--, i++);
+		}
+
+		return true;
+	}
+
+	private static int check() {
+		List<Integer> AList = new ArrayList<>();
+		List<Integer> BList = new ArrayList<>();
+
+		int a = 0;
+		int b = 0;
+
+		for (int i = 0; i < N; ++i) {
+			if (selected[i] == 1)
+				AList.add(i);
+			else
+				BList.add(i);
+		}
+
+		for (int i = 0; i < N / 2; ++i) {
+			for (int j = i + 1; j < N / 2; ++j) {
+				a += (S[AList.get(i)][AList.get(j)] + S[AList.get(j)][AList.get(i)]);
+				b += (S[BList.get(i)][BList.get(j)] + S[BList.get(j)][BList.get(i)]);
+			}
+		}
+
+		return Math.abs(a - b);
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		S = new int[N][N];
+		answer = Integer.MAX_VALUE;
+		selected = new int[N];
+
+		for (int i = 0; i < N; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; ++j) {
+				S[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int idx = N - 1;
+		while (idx >= N / 2) {
+			selected[idx--] = 1;
+		}
+
+		do {
+			answer = Math.min(check(), answer);
+		} while (nextPermutation(selected));
+
+		System.out.println(+answer);
+
+	}
 }
+
--- problems/programmers/후보키_2019_KAKAO_BLIND.java
+++ problems/programmers/후보키_2019_KAKAO_BLIND.java
@@ -1,5 +1,92 @@
 package problems.programmers;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class 후보키_2019_KAKAO_BLIND {
 
+	public void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public boolean nextPermutation(int[] arr, int N) {
+		int i = N - 1;
+		while (i > 0 && arr[i - 1] >= arr[i])
+			--i;
+
+		if (i == 0)
+			return false;
+
+		int j = N - 1;
+		while (arr[i - 1] >= arr[j])
+			--j;
+
+		swap(arr, i - 1, j);
+
+		int k = N - 1;
+		while (k > i) {
+			swap(arr, i++, k--);
+		}
+
+		return true;
+	}
+
+	public int solution(String[][] relation) {
+		int answer = 0;
+		int size = relation[0].length;
+		List<int[]> list = new ArrayList<>();
+		
+		for (int i = 1; i <= size; ++i) {
+			int[] selected = new int[size];
+			for (int j = size - 1; j >= size - i; --j)
+				selected[j] = 1;
+			
+			
+			do {
+				Set<String> set = new HashSet<>();
+				boolean flag = true;
+				
+				for(int j = 0; j<list.size(); ++j) {
+					int[] arr = list.get(j);
+					int sameCount = 0;
+					int oneCount = 0;
+					for(int k = 0; k<arr.length; ++k) {
+						oneCount = (arr[k] == 1)? oneCount+1 : oneCount;
+						sameCount = (selected[k] + arr[k] == 2)? sameCount+1:sameCount;
+					}
+					if(oneCount == sameCount) {
+						flag = false;
+						break;
+					}
+				}
+				
+				for (int j = 0; j < relation.length && flag; ++j) {
+					String str = "";
+					for (int k = 0; k < relation[j].length; ++k) {
+						if (selected[k] == 1) {
+							str += relation[j][k];
+						}
+					}
+					if(!set.add(str))
+						flag = false;
+				}
+				if(flag) {
+					list.add(selected.clone());
+					++answer;
+				}
+
+			}while (nextPermutation(selected, size));
+
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int a = new 후보키_2019_KAKAO_BLIND().solution(new String[][] {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}});
+		System.out.println(a);
+	}
 }
